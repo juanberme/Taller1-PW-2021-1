e11f2e41e59e042cbd3ce7d02c3bae8318e3c79f
juan camilo bermeo <camilo22601@gmail.com>
2021-03-18 01:20:29 -0500

    diesiete

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 let sparkle5 = document.querySelector('.sparkle5');
 let slide_info = document.querySelector('.slide_info');
+let slide_button = document.querySelector('.slide_button');
+let reason_title = document.querySelector('.reason_title');
 let why_info = document.querySelector('.why_info');
 
 function handleMoveSparkle5() {
     console.log(sparkle5.scrollX);
-    if(window.scrollY > slide_info.offsetTop){
-        sparkle5.style.transform = 'translate( -'+ (window.scrollY - slide_info.offsetTop) +'px, 0px)';
-        if( window.scrollY < why_info.offsetTop){
-            
+    if(window.scrollY > slide_button.offsetTop){  
+        if( window.scrollY < reason_title.offsetTop){
+            sparkle5.style.transform = 'translate( -'+ (window.scrollY - slide_button.offsetTop) +'px, 0px)';
         }
         
     }
