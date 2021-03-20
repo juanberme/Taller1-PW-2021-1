0f0fca6abe32fad54559cecf2da957ea6aa8301e
juan camilo bermeo <camilo22601@gmail.com>
2021-03-19 22:43:42 -0500

    veintidos

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let menuPic = document.querySelector('.menuPic');
 let option2 = document.querySelector('.option2');
-
+//funcion para hacer el menú desplegable
 function handleAppearNewMenu(){
     if(option2.style.display === 'none'){
         option2.style.display = 'flex';
@@ -8,12 +8,11 @@ function handleAppearNewMenu(){
         option2.style.display = 'none';
     };
 }
-
 menuPic.addEventListener('click', handleAppearNewMenu);
 
-var counter = 1;
 
-//
+var counter = 1;
+//funcion para hacer que el slide se mueva
 setInterval(function(){
     document.getElementById('radio'+ counter).checked = true;
     counter++;
@@ -22,27 +21,28 @@ setInterval(function(){
     }
 }, 5000);
 
+
+//pepas
 let sparkle2 = document.querySelector('.sparkle2');
 let sparkle3 = document.querySelector('.sparkle3');
+let sparkle4 = document.querySelector('.sparkle4');
 let sparkle5 = document.querySelector('.sparkle5');
+
 let slide_info = document.querySelector('.slide_info');
 let slide_button = document.querySelector('.slide_button');
 let reason_title = document.querySelector('.reason_title');
 let reason = document.querySelector('.reason');
 let why_info = document.querySelector('.why_info');
-
+//funcion para mover las chispas
 function handleMoveSparkle5() {
     //console.log(window.scrollY, slide_info.offsetTop, reason.offsetTop);
     if(window.scrollY > slide_info.offsetTop){  
         if( window.scrollY < reason.offsetTop){
-            sparkle2.style.transform = 'translate( -'+ ((window.scrollY - slide_info.offsetTop))+'px,-'+(((window.scrollY + slide_info.offsetTop)-1012)/3)+'px)';
-            sparkle3.style.transform = 'translate( -'+((window.scrollY - slide_info.offsetTop))+'px,-'+(((window.scrollY + slide_info.offsetTop)-1175)/2)+'px)';
-            sparkle5.style.transform = 'translate( -'+ ((window.scrollY - slide_info.offsetTop)) +'px,'+(((window.scrollY + slide_info.offsetTop)-1158)/4)+'px)';
-        }
-        
-    }
-    
-    
+            sparkle2.style.transform = 'translate(-'+((window.scrollY)/1.5)+'px,-'+((window.scrollY)/9)+'0px)';
+            sparkle3.style.transform = 'translate( -'+((window.scrollY)/2)+'px,'+((window.scrollY)/8)+'px)';
+            sparkle4.style.transform = 'translate(-'+((window.scrollY)/3)+'px, '+((window.scrollY)/15)+'px)';
+            sparkle5.style.transform = 'translate( -'+(window.scrollY/4)+'px, '+((window.scrollY)/1.7)+'px)';
+        } 
+    } 
 }
-
 window.addEventListener('scroll', handleMoveSparkle5);
